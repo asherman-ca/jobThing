16d203e2d1b8eb17b1cc6e4acca218ae9c6f7cd6
Alex Sherman <asherman.ca@gmail.com>
2023-05-07 22:45:13 -0700

    nav logo

--- app/components/nav/Nav.tsx
+++ app/components/nav/Nav.tsx
@@ -1,13 +1,18 @@
 'use client'
 import { useState } from 'react'
+import Image from 'next/image'
+import resume from '@/public/resume.png'
 
 const Nav = () => {
 	const [effect, setEffect] = useState(false)
 	return (
-		<div className='bg-mainbg flex justify-between p-8'>
-			<div className='text-2xl font-bold'>jobThing</div>
+		<div className='bg-mainbg flex justify-between p-4'>
+			<div className='text-2xl font-bold flex gap-4 items-center'>
+				<Image src={resume} alt='resume' className='h-10 w-10' />
+				jobThing
+			</div>
 			<button
-				className={`rounded-sm bg-white px-4 py-2 text-mainbg font-bold text-lg transition-all duration-200 hover:text-white hover:bg-green-500 hover:scale-110 ${
+				className={`rounded-sm bg-white px-4 py-2 text-mainbg font-bold text-lg transition-all duration-200 hover:text-white hover:bg-green-500 hover:scale-105 ${
 					effect && 'animate-wiggle'
 				}`}
 				onClick={() => {
