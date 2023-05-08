9b9e77ca0c135ee4a80abd60f9ccf0502efdec50
Alex Sherman <asherman.ca@gmail.com>
2023-05-07 22:25:51 -0700

    init

---
+++ app/components/nav/Nav.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Nav = () => {
+	return (
+		<div className='flex justify-between p-8'>
+			<div>jobThing</div>
+			<div>Sign In</div>
+		</div>
+	)
+}
+
+export default Nav
